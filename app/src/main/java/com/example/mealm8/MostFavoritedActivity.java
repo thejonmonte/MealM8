b48020d4d65152124853c54682c552e362a0f89e
Jonathan Montemayor <jmontema@usc.edu>
2019-11-14 23:30:18 -0800

    Attempting JDBC

--- app/src/main/java/com/example/mealm8/MostFavoritedActivity.java
+++ app/src/main/java/com/example/mealm8/MostFavoritedActivity.java
@@ -1,13 +1,17 @@
 package com.example.mealm8;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MostFavoritedActivity extends AppCompatActivity {
+//    private ArrayList<ClassListItems> itemArrayList;
+//    private MyAppAdapter myAppAdapter; // Array Adapter
+//    private ListView listView;
+    private boolean success = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
 }
