3ba599886bf5cb75d01aac63a11b97a6e52c6666
abotansky <botansky@usc.edu>
2019-11-14 19:36:13 -0800

    xml for recipe details

---
+++ app/src/main/java/com/example/mealm8/RecipeDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mealm8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+    }
+}
