d9ebd6aad2022f69b3b72ab6fb7f2e1f3d690a8d
Jonathan Montemayor <jmontema@usc.edu>
2019-11-14 21:25:40 -0800

    Database

---
+++ app/src/main/java/com/example/mealm8/MostFavoritedActivity.java
@@ -0,0 +1,13 @@
+package com.example.mealm8;
+
+import android.os.Bundle;
+
+public class MostFavoritedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+}
