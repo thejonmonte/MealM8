a0bcbabad22d4965adf0838f7c37f369a632969d
Izzy Lau <laui@usc.edu>
2019-11-14 21:16:15 -0800

    Created LoginActivity Page. Added dummy button variables to serve as placeholders before Google Auth0 is incorporated.

---
+++ app/src/main/java/com/example/mealm8/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.mealm8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // gets basic user info and email
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//
+//        // Build a GoogleSignInClient with the options specified by gso.
+//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+}
